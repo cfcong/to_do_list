7078107e4c943d52a82ad40f216f612841f8747b
Colin Fcong <cfcong@gmail.com>
2013-11-06 06:40:20 +0000

    styled a bit

--- todolist.js
+++ todolist.js
@@ -3,21 +3,16 @@ $(function() {
     $('#addInformation').on('click', function(){
         //when clicking button "addInformation", store the string
         var enteredAnswer = $('#enterInformation').val();
-        var button = $('<button class="btn-remove">X</button>');
+        var button = $('<button class="btn-remove">Delete</button>');
         //from there, take the store string and create a new line (in the unordered list) for it
-        $('#information').append($('<li></li>').append(enteredAnswer).append(button));
-        //var newLi = $("<li>olives</li>").hide();
-        //var btn = $('<button class="btn-remove">Delete</button>');
-        $('#enterInformation').val(' ');
         //for the entered string, add a button for delete
-    	//access the list and add a new line
-    	//transition the line into the list
-
+        //access the list and add a new line
+        $('#information').append($('<li></li>').append(enteredAnswer).append(button));
+        //clear the pform when submitting information
+        $('#enterInformation').val(' ');
     });
 
     $('#information').on('click', '.btn-remove', function(){ 
-        
         $(this).closest('li').fadeOut();
-   
     });    
 });
