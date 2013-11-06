f49034996211bf58d87dad75fcef45a388b1e411
Colin Fcong <cfcong@gmail.com>
2013-11-06 05:54:04 +0000

    delete button added and functional

--- todolist.js
+++ todolist.js
@@ -3,8 +3,9 @@ $(function() {
     $('#addInformation').on('click', function(){
         //when clicking button "addInformation", store the string
         var enteredAnswer = $('#enterInformation').val();
+        var button = $('<button class="btn-remove">X</button>');
         //from there, take the store string and create a new line (in the unordered list) for it
-        $('#information').append($('<li></li>').append(enteredAnswer));
+        $('#information').append($('<li></li>').append(enteredAnswer).append(button));
         //var newLi = $("<li>olives</li>").hide();
         //var btn = $('<button class="btn-remove">Delete</button>');
         $('#enterInformation').val(' ');
@@ -14,9 +15,9 @@ $(function() {
 
     });
 
-    //$('#information').on('click', '.btn-remove', function(){ 
+    $('#information').on('click', '.btn-remove', function(){ 
         
-        //$(this).closest('li').fadeOut();
+        $(this).closest('li').fadeOut();
    
-    //});    
+    });    
 });
